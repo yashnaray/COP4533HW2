d34f3de81e022bfc3e729b10c7483a1b6b2fa370
Yash Narayan <dop422657@gmail.com>
2026-03-04 09:47:06 -0500

    Added other caches

--- cache.py
+++ cache.py
@@ -21,3 +21,55 @@ class FIFOCache:
     
     def get_stats(self):
         return self.hits, self.misses
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = {}
+        self.hits = 0
+        self.misses = 0
+        self.time = 0
+    
+    def request(self, item):
+        self.time += 1
+        if item in self.cache:
+            self.hits += 1
+            self.cache[item] = self.time
+        else:
+            self.misses += 1
+            if len(self.cache) >= self.capacity:
+                lru_item = min(self.cache, key=self.cache.get)
+                del self.cache[lru_item]
+            self.cache[item] = self.time
+    
+    def get_stats(self):
+        return self.hits, self.misses
+
+class OPTFFCache:
+    def __init__(self, capacity, requests):
+        self.capacity = capacity
+        self.cache = set()
+        self.hits = 0
+        self.misses = 0
+        self.requests = requests
+        self.index = 0
+    
+    def request(self, item):
+        if item in self.cache:
+            self.hits += 1
+        else:
+            self.misses += 1
+            if len(self.cache) >= self.capacity:
+                farthest = max(self.cache, key=lambda x: self._next_use(x))
+                self.cache.remove(farthest)
+            self.cache.add(item)
+        self.index += 1
+    
+    def _next_use(self, item):
+        for i in range(self.index + 1, len(self.requests)):
+            if self.requests[i] == item:
+                return i
+        return float('inf')
+    
+    def get_stats(self):
+        return self.hits, self.misses
